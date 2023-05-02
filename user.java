05d551545d82fed366d5741bff6e5a21ca170380
Fatima Adel <132399088+FatimaADEl17@users.noreply.github.com>
2023-05-02 23:18:13 +0300

    Add files via upload

---
+++ user.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hp
+ */
+public class user {
+    private String FirstName;
+    private String LastName;
+    private String UserName;
+    private String PhoneNumber;
+    private String Email;
+    private String Passowrd;
+    private String ConfirmPassword;
+    private int id ;
+    private String Status;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassowrd() {
+        return Passowrd;
+    }
+
+    public void setPassowrd(String Passowrd) {
+        this.Passowrd = Passowrd;
+    }
+
+    public String getConfirmPassword() {
+        return ConfirmPassword;
+    }
+
+    public void setConfirmPassword(String ConfirmPassword) {
+        this.ConfirmPassword = ConfirmPassword;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+    
+
+}
